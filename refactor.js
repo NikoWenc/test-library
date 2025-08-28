0e1c67b015f51a5913e1fff8ec48e072215fa6d2
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-08-28 21:50:53 +0800

    add append method to BookEntry and listeners to modal buttons

--- refactor.js
+++ refactor.js
@@ -1,24 +1,75 @@
 
+function globalAddEvent(element, action, func) {
+    return element.addEventListener(action, func);
+}
+
+function createElement (tag, text, classList) {
+    const element = document.createElement(tag);
+    if (text) element.textContent = text;
+    if (classList) element.classList.add(classList);
+    return element;
+}
+
 class BookEntry {
 
     constructor(book) {
-        const newDiv = createElement('div', false, 'center');
-        const bookTitle = createElement('h2', `Book Title: ${book.title}`);
-        const bookAuthor = createElement('p', `Book Author: ${book.author}`);
-        const bookPages = createElement('p', `Number of Pages: ${book.pages}`);
-        const bookStatus = createElement('p', book.status);
-        const buttonsDiv = createElement('div', false, 'buttonsDiv');
-        const readButton = createElement('button', 'Done', 'doneBTN');
-        const delButton = createElement('button', 'Delete', 'delBTN');
+        this.newDiv = createElement('div', false, 'center');
+        this.bookTitle = createElement('h2', `Book Title: ${book.title}`);
+        this.bookAuthor = createElement('p', `Book Author: ${book.author}`);
+        this.bookPages = createElement('p', `Number of Pages: ${book.pages}`);
+        this.bookStatus = createElement('p', book.status);
+        this.buttonsDiv = createElement('div', false, 'buttonsDiv');
+        this.readButton = createElement('button', 'Done', 'doneBTN');
+        this.delButton = createElement('button', 'Delete', 'delBTN');
+
+        // change status when done reading
+        readButton.addEventListener('click', () => {
+            book.doneRead();
+            bookStatus.textContent = book.status;
+            bookStatus.style.color = 'green';
+        })
+
+        // delete the entry
+        delButton.addEventListener('click', () => {
+            container.removeChild(newDiv)
+            const index = myLibrary.findIndex(elementIndex => elementIndex.id === book.id);
+            if (index != -1) myLibrary.splice(index, 1);
+        });
     }
 
-    // helper method to create DOM elements
-    static createElement (tag, text, classList) {
-        const element = document.createElement(tag);
-        if (text) element.textContent = text;
-        if (classList) element.classList.add(classList);
-        return element;
+    append(){
+        buttonsDiv.append(
+            readButton, 
+            delButton
+        );
+        return newDiv.append(
+            bookTitle, 
+            bookAuthor, 
+            bookPages, 
+            bookStatus, 
+            buttonsDiv,
+        );
+    }
+    
+
+}
+
+const bookEntry = new BookEntry();
+
+class Book {
+    constructor(title, author, pages){
+        this.status = 'not read yet';
+        this.title;
+        this.author;
+        this.pages;
+        this.id = rypto.randomUUID();
     }
 
+    doneRead(){
+        return this.status = 'Finished';
+    }
 
+    info(){
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`;
+    }
 }
